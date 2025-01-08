b45c1129c011adf65bdf0ea2d07164afc0af8603
wafiyaalim <144928869+wafiyaalim@users.noreply.github.com>
2025-01-08 18:20:59 +0530

    change

---
+++ lib/screens/lib/l10n/app_en.arb.dart
@@ -0,0 +1,28 @@
+{
+"Title": "RailRelax",
+"@Title": {
+"description": "The application name"
+},
+"menu": "Menu",
+"@menu": {
+"description": "Menu text in drawer"
+},
+"home": "Home",
+"trackTrains": "Track Trains",
+"liveChat": "Live Chat",
+"stationMap": "Station Map",
+"newsAlerts": "News & Alerts",
+"helpline": "Helpline",
+"faq": "FAQ",
+"shareWithFriends": "Share with Friends",
+"contactUs": "Contact Us",
+"changeLanguage": "Change Language",
+"termsConditions": "Terms & Conditions",
+"search": "Search...",
+"trainTracking": "Train Tracking",
+"buyTicket": "Buy Ticket",
+"news": "News",
+"other": "Other",
+"policeStationLocator": "Police Station Locator",
+"fareCalculator": "Fare Calculator"
+}
