e87b4c24dac50728b36eff73e5e341d989620014
wafiyaalim <144928869+wafiyaalim@users.noreply.github.com>
2025-01-13 12:10:15 +0530

    change

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'screens/lib/screens/splash.dart';// Adjust the import patt
-import 'package:get/get.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +8,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: SplashScreenPage(),
     );
